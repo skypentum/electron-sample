40bf5c554f55c07dfc7d0cbe618471d8a2dcc62a
Simon Kim <skypentum@gmail.com>
2018-10-31 02:05:47 +0900

    - drag 기능 추가

--- renderer.js
+++ renderer.js
@@ -2,5 +2,60 @@
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
 $( function() {
-    $( "#draggable" ).draggable();
+    $( "#drag-form" ).draggable({ 
+        opacity: 0.7, 
+        helper: "clone",
+        stop: function() {
+            let result = '<form method="post"></form>'
+            $('.render-page').append(result)
+        } 
+    })
+    $( "#drag-text" ).draggable({ 
+        opacity: 0.7, 
+        helper: "clone",
+        stop: function() {
+            let result = '<input type="text" name="test-text">'
+            if($('.render-page').find('form') == undefined) {
+                $('.render-page').append(result)
+            } else {
+                $('.render-page form').append(result)
+            }     
+        } 
+    })
+    $( "#drag-textarea" ).draggable({ 
+        opacity: 0.7, 
+        helper: "clone",
+        stop: function() {
+            let result = '<textarea name="test-textarea"></textarea>'
+            if($('.render-page').find('form') == undefined) {
+                $('.render-page').append(result)
+            } else {
+                $('.render-page form').append(result)
+            }     
+        } 
+    })
+    $( "#drag-label" ).draggable({ 
+        opacity: 0.7, 
+        helper: "clone",
+        stop: function() {
+            let result = '<p name="test-label">label</p>'
+            if($('.render-page').find('form') == undefined) {
+                $('.render-page').append(result)
+            } else {
+                $('.render-page form').append(result)
+            }     
+        } 
+    })
+    $( "#drag-button" ).draggable({ 
+        opacity: 0.7, 
+        helper: "clone",
+        stop: function() {
+            let result = '<button>submit</button>'
+            if($('.render-page').find('form') == undefined) {
+                $('.render-page').append(result)
+            } else {
+                $('.render-page form').append(result)
+            }     
+        } 
+    })
 })
